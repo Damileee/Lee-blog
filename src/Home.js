aea1c7baaa99a5923dc91a910aa371ba298b1a08
Damileee <olasojudamilare6@gmail.com>
2023-10-15 13:24:03 +0100

    Handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -2,33 +2,43 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-    // const handleDelete = (id) => {
-    //     const newBlogs = blogs.filter(blog => blog.id !== id);
-    //     setBlogs(newBlogs);
-    // }
+  // const handleDelete = (id) => {
+  //     const newBlogs = blogs.filter(blog => blog.id !== id);
+  //     setBlogs(newBlogs);
+  // }
 
-    useEffect(() => {
-       setTimeout(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if(!res.ok) {
+            throw Error('could not fetch the data from that resource');
+          }
           return res.json();
         })
-        .then(data => {
+        .then((data) => {
           setBlogs(data);
           setIsPending(false);
-        });
-       }, 1000);
-    }, []);
-  
-    return (
-      <div className="home">
-        { isPending && <div>Loading...</div> }
-       {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
-      </div>
-    );
-  }
-   
-  export default Home;
+          setError(null);
+        })
+        .catch(err => {
+          setIsPending(false);
+          setError(err.message);
+        })
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="home">
+      {error && <div>{ error }</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
+  );
+};
+
+export default Home;

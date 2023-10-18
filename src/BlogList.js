f8b29cc578249e13ee34caf582ef580494b97192
Damileee <olasojudamilare6@gmail.com>
2023-10-18 17:05:37 +0100

    learned: Routing

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
 
   return (
@@ -5,6 +7,7 @@ const BlogList = ({ blogs, title }) => {
         <h1>{ title }</h1>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
+          <Link to={`/blogs/${blog.id}`}></Link>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
         </div>
